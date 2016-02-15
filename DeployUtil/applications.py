438dbf61139498027bd5d43355a023434cd6f62d
Larry Olson <loarabia@hotmail.com>
2016-02-15 09:09:30 -0800

    Cleanup application handler
    Remove debugging logic

--- DeployUtil/applications.py
+++ DeployUtil/applications.py
@@ -3,15 +3,12 @@ import ssl
 import http.cookiejar
 
 def do_list(ip, pretty, **_args):
-	print(ip)
-	print(pretty)
 	scheme = 'https://'
 	port = ''
 	api = '/api/app/packagemanager/packages?'
 	verb = 'GET'
 
 	request_url = scheme + ip + port + api
-	print(request_url)
 	request = urllib.request.Request(url=request_url, method=verb)
 
 	context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
@@ -20,7 +17,5 @@ def do_list(ip, pretty, **_args):
 
 	cookies = urllib.request.HTTPCookieProcessor(http.cookiejar.MozillaCookieJar("deployUtil.cookies"))
 	cookies.cookiejar.load(ignore_discard=True)
-	print(cookies.cookiejar)
 	opener = urllib.request.build_opener(https_handler, cookies)
 	resp = opener.open(request)
-	print(resp.read())
