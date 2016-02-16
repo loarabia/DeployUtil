6bdfb7d3dfc9804e2b81dab12515b1147dd657a9
Larry Olson <loarabia@hotmail.com>
2016-02-15 20:15:39 -0800

    Make application commands us the requests module
    Removed the tools session code since it wasn't needed any longer because the
    requests module handles everything it did or was planned to do.

--- DeployUtil/applications.py
+++ DeployUtil/applications.py
@@ -1,23 +1,28 @@
-import urllib.request
-import http.cookiejar
-import DeployUtil.toolsession as session
 import json
+import requests
 
+#TODO What happens if I don't have a cookie? There are a few possibilities.
+#	1. Authentication is turned off and then probably I don't care
+#	2. Authentication isn't cookie based. Need more logic
+#	3. There is an error, so I really need to go back and tell the user get
+#		a cookie if they can
+#	4. There was some error in the session (network timeout, file read
+#		failure, etc.) Let the user know and exit.
 def do_list(ip, **_args):
 	scheme = 'https://'
 	port = ''
 	api = '/api/app/packagemanager/packages?'
-	verb = 'GET'
-
 	request_url = scheme + ip + port + api
-	request = urllib.request.Request(url=request_url, method=verb)
 
-	https_handler = session.create_toolsess_httpsHandler()
+	with requests.Session() as session:
+		cookie_filename = 'deployUtil.cookies'
+		with open(cookie_filename,'r') as cookie_file:
+			cookies = json.load(cookie_file)
+			response = session.get(	request_url,
+						verify=False,
+						cookies=cookies)
+			json_obj = json.loads(response.text)
+			return json_obj
 
-	cookies = urllib.request.HTTPCookieProcessor(http.cookiejar.MozillaCookieJar("deployUtil.cookies"))
-	cookies.cookiejar.load(ignore_discard=True)
-	opener = urllib.request.build_opener(https_handler, cookies)
-	resp = opener.open(request)
-
-	json_obj = json.loads(resp.read().decode())
-	return json_obj
+def do_install(ip, appx, deps, **_args):
+	pass
--- DeployUtil/toolsession.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import ssl
-
-def create_toolsess_httpsHandler():
-	context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-	context.verify_mode = ssl.CERT_NONE
-	https_handler = urllib.request.HTTPSHandler(context=context)
-	return https_handler
