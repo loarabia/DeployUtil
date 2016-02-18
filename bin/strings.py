cdc811e575a560d3f8b98f3d2aea1cbb91605915
Larry Olson <loarabia@hotmail.com>
2016-02-17 22:32:16 -0800

    Fixing some Pylint complaints with strings module
    Some of the naming I'm not sure I agree with yet...

--- bin/strings.py
+++ bin/strings.py
@@ -1,51 +1,55 @@
+#This code is licensed under the "MIT License" see LICENSE.txt
+""" This contains all of the definitions for the commandline arguments as
+named tuples.
+"""
 from collections import namedtuple
 
-opt = namedtuple("opt",['switch','help'])
+argument = namedtuple("argument", ['switch', 'help'])
 
 # common options
-ip = opt(	switch='-ip',
-		help='ip args help')
+ip = argument(switch='-ip',
+              help='ip args help')
 
 # pair command and options
-pair = opt(	switch='pair',
-		help='pair command help')
+pair = argument(switch='pair',
+                help='pair command help')
 
-pin = opt(	switch='-pin',
-		help='pin arg help')
+pin = argument(switch='-pin',
+               help='pin arg help')
 
 # list command and options
-list = opt(	switch='list',
-		help='list command help')
+list = argument(switch='list',
+                help='list command help')
 
-pretty = opt(	switch='-pretty',
-		help='pretty arg help')
+pretty = argument(switch='-pretty',
+                  help='pretty arg help')
 
 # install command and options
-inst = opt(	switch='install',
-		help='install command help')
+inst = argument(switch='install',
+                help='install command help')
 
-deps = opt(	switch='--depsdir',
-		help='dependencies directory help')
+deps = argument(switch='--depsdir',
+                help='dependencies directory help')
 
-appx = opt(	switch='appx',
-		help='appx help')
+appx = argument(switch='appx',
+                help='appx help')
 
 # uninstall command and options
-uninst = opt(	switch='uninstall',
-		help='uninstall command help')
+uninst = argument(switch='uninstall',
+                  help='uninstall command help')
 
-pfn = opt(	switch='-pfn',
-		help='package full name arg help')
+pfn = argument(switch='-pfn',
+               help='package full name arg help')
 
-prid = opt(	switch='-prid',
-		help='package relateve id help')
+prid = argument(switch='-prid',
+                help='package relateve id help')
 
 # launch command and options
-launch = opt(	switch='launch',
-		help='launch command help ')
+launch = argument(switch='launch',
+                  help='launch command help ')
 
 tool_desc = \
 """ Provides command line access to developer features exposed by the Windows
 Device Portal including, pairing with devices, managing applications, and
 managing processes on devices.
-""" 
+"""
